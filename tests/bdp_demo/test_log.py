48f5c9ddad960394b3a0f607fa95dba1974051ab
Ian McGinnis <ian.mcginnis@noaa.gov>
2021-08-04 17:27:09 -0600

    Use boto's builtin stub to intercept actual calls
    Our test machines won't be logged in to an AWS account.

--- tests/bdp_demo/test_log.py
+++ tests/bdp_demo/test_log.py
@@ -4,24 +4,53 @@ from bdp_demo import handlers
 
 import pytest
 import sys
+from botocore.stub import Stubber
+import boto3
 
 
 # Arrange
+@pytest.fixture(scope="function")
+def s3_client():
+    s3 = boto3.client("s3")
+    return s3
+
+
+@pytest.fixture(scope="function")
+def s3_stub(s3_client, autouse=True):
+    with Stubber(s3_client) as stubber:
+        yield stubber
+        stubber.assert_no_pending_responses()
+
+
 @pytest.fixture
-def logger():
+def logger(s3_stub, s3_client):
     # TODO: decouple this
     handler = handlers.TextHandler(sys.stdout)
-    s3 = storage_layer.S3Storage()  # TODO - use a FileStorage instead?
+    s3 = storage_layer.S3Storage(s3_client)  # TODO - use a FileStorage instead?
     logger = log.Logger(storage_layer=s3, handlers=[handler])
     return logger
 
 
-def test_log_bad_resource(logger):
+def test_log_bad_resource(logger, s3_stub):
+    s3_stub.activate()
+    # Mock checking for download
+    s3_stub.add_response(
+        "head_bucket",
+        expected_params={"Bucket": "foo"},
+        service_response={},
+    )
+    s3_stub.add_client_error(
+        method="head_object",
+        service_error_code="404",
+        service_message="""
+        The specified key does not exist.
+        """,
+    )
     with pytest.raises(SystemExit):
         logger.log("s3://foo/bar.baz")
 
 
-def test_log_unsupported_storage(logger):
+def test_log_unsupported_storage(logger, s3_stub):
     with pytest.raises(SystemExit):
         logger.log("foo://bar.baz")
 
--- tests/bdp_demo/test_storage_layer.py
+++ tests/bdp_demo/test_storage_layer.py
@@ -1,48 +1,66 @@
 from bdp_demo import storage_layer
 
+import pytest
+from botocore.stub import Stubber
+import boto3
 
-def test_scheme():
-    s3 = storage_layer.S3Storage()
+
+# Arrange
+@pytest.fixture(scope="function")
+def s3_client():
+    s3 = boto3.client("s3")
+    return s3
+
+
+@pytest.fixture(scope="function")
+def s3_stub(s3_client, autouse=True):
+    with Stubber(s3_client) as stubber:
+        yield stubber
+        stubber.assert_no_pending_responses()
+
+
+def test_scheme(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.scheme() == "s3://"
 
 
-def test_supports():
-    s3 = storage_layer.S3Storage()
+def test_supports(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.supports("s3://")
 
 
-def test_supports_fail():
-    s3 = storage_layer.S3Storage()
+def test_supports_fail(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert not s3.supports("file://")
 
 
-def test_guess_mtype_grib2():
-    s3 = storage_layer.S3Storage()
+def test_guess_mtype_grib2(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.guess_mtype("s3://foo/bar.grib2") == "application/x-grib2"
 
 
-def test_guess_mtype_spec():
-    s3 = storage_layer.S3Storage()
+def test_guess_mtype_spec(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.guess_mtype("s3://foo/bar.spec") == "text/plain"
 
 
-def test_guess_mtype_bull():
-    s3 = storage_layer.S3Storage()
+def test_guess_mtype_bull(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.guess_mtype("s3://foo/bar.bull") == "text/plain"
 
 
-def test_guess_mtype_cbull():
-    s3 = storage_layer.S3Storage()
+def test_guess_mtype_cbull(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.guess_mtype("s3://foo/bar.cbull") == "text/plain"
 
 
-def test_guess_mtype_tar():
-    s3 = storage_layer.S3Storage()
+def test_guess_mtype_tar(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.guess_mtype("s3://foo/bar.tar") == "application/x-tar"
 
 
-def test_guess_mtype_unknown():
-    s3 = storage_layer.S3Storage()
+def test_guess_mtype_unknown(s3_stub, s3_client):
+    s3 = storage_layer.S3Storage(s3_client)
     assert s3.guess_mtype("s3://foo/bar.baz") is None
 
 
